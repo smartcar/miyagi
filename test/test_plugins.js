643748aa067c956199e5601771ef0393c64b41dd
Max O'Connell <moconnell7171@gmail.com>
2017-01-12 19:21:12 +0000

    Create and test 'pull' method

--- test/test_plugins.js
+++ test/test_plugins.js
@@ -36,3 +36,44 @@ test.cb('replace modifies files', function(t) {
     .pipe(end(t.end));
 
 });
+
+test.cb('pull pipes tag content to the next plugin', function(t) {
+  const verify = function(file) {
+    t.is(file, mock.originalScript);
+  };
+
+  gulp.src(path.join(moduleDirectory, 'fixtures/original.html'))
+    .pipe(plugin.pull('script'))
+    .pipe(check(verify))
+    .pipe(end(t.end));
+
+});
+
+test.cb('pull will create a new stream for every matching tag pair', function(t) {
+  var completedStreams = 0;
+
+  const verify = function(file) {
+    completedStreams++;
+
+    if (file !== mock.originalScript) {
+      t.is(file, mock.modifiedScript);
+    }
+
+    if (completedStreams === 2) {
+      t.end();
+    }
+  };
+
+  t.plan(1);
+
+  /*
+  * The 'multiple-match.html' file contains two 'script' tags.
+  * When passed into 'pull', it will be split into two separate
+  * streams. One will be identical to 'original-script.js'. The
+  * other will match 'modified-script.js'
+  */
+  gulp.src(path.join(moduleDirectory, 'fixtures/multiple-match.html'))
+    .pipe(plugin.pull('script'))
+    .pipe(check(verify));
+
+});
